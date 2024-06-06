ff7c92eee12462d7aca91ea78ab15df4c28d990e
FlavioAraujo23 <flaviolopes1027@gmail.com>
2024-06-06 19:55:33 -0300

    add submit function to hook use-sign-up

--- src/hooks/sign-up/use-sign-up.ts
+++ src/hooks/sign-up/use-sign-up.ts
@@ -8,6 +8,7 @@ import { useSignUp } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { onCompleteUserRegistration } from '@/actions/auth';
 
 export const useSignUpForm = () => {
   const { toast } = useToast();
@@ -21,4 +22,75 @@ export const useSignUpForm = () => {
     },
     mode: 'onChange',
   });
+
+  const onGenerateOTP = async (
+    email: string,
+    password: string,
+    onNext: React.Dispatch<React.SetStateAction<number>>
+  ) => {
+    if (!isLoaded) return;
+
+    try {
+      await signUp.create({
+        emailAddress: email,
+        password: password,
+      });
+
+      await signUp.prepareEmailAddressVerification({ strategy: 'email_code' });
+
+      onNext((prev) => prev + 1);
+    } catch (error: any) {
+      toast({
+        title: 'Erro',
+        description: error.erros[0].longMessage,
+      });
+    }
+  };
+
+  const onHandleSubmit = methods.handleSubmit(
+    async (values: UserRegistrationProps) => {
+      if (!isLoaded) return;
+
+      try {
+        setLoading(true);
+        const completeSignUp = await signUp.attemptEmailAddressVerification({
+          code: values.otp,
+        });
+
+        if (completeSignUp.status !== 'complete') {
+          return { message: 'Alguma coisa deu errado!' };
+        }
+
+        if (completeSignUp.status === 'complete') {
+          if (!signUp.createdUserId) return;
+
+          const registered = await onCompleteUserRegistration(
+            values.fullname,
+            signUp.createdUserId,
+            values.type
+          );
+
+          if (registered?.status === 200 && registered.user) {
+            await setActive({
+              session: completeSignUp.createdSessionId,
+            });
+            setLoading(false);
+            router.push('/dashboard');
+          }
+
+          if (registered?.status === 400) {
+            toast({
+              title: 'Erro',
+              description: 'Alguma coisa deu errado',
+            });
+          }
+        }
+      } catch (error: any) {
+        toast({
+          title: 'Error',
+          description: error.errors[0].longMessage,
+        });
+      }
+    }
+  );
 };
